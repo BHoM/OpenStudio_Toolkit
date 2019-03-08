e7a9a2e6e139102951b8c56e7a99c0eca54709fb
Tristan Gerrish <tristan.gerrish@burohappold.com>
2019-03-08 13:32:19 +0000

    Cartesian point/vector modifications

---
+++ EnergyPlus_Engine/Modify/OffsetCurve.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClipperLib;
+
+using BHG = BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BHE = BH.oM.Environment.Elements;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.EnergyPlus
+{
+    public static partial class Modify
+    {
+        [Description("Offsets a planar 3D polyline, translating it to 2D for offseting, then re-translating the offset to 3D")]
+        [Input("tbdPolygon", "TAS TBD Polygon")]
+        [Output("BHoM Geometry Polyline")]
+        public static BHG.PolyCurve OffsetCurve(BHG.PolyCurve polycurve, double offsetDistance)
+        {
+            //Clipper.OffsetPolygons(new List<List<ClipperLib.IntPoint>>(), 0.1, JoinType.jtSquare);
+            //List<BHG.Point> childpts = child.OpeningCurve.IDiscontinuityPoints();
+            return new BHG.PolyCurve();
+        }
+
+        public static BHG.Vector UnitVector(BHG.Vector A, BHG.Vector B)
+        {
+            BHG.Vector distanceBetween = CartesianSubtraction(B, A);
+            return distanceBetween / Math.Sqrt(CartesianSum(CartesianMultiplication(distanceBetween, distanceBetween)));
+        }
+
+        public static double CartesianSum(BHG.Vector A)
+        {
+            return A.X + A.Y + A.Z;
+        }
+
+        public static double CartesianSum(BHG.Point A)
+        {
+            return A.X + A.Y + A.Z;
+        }
+
+
+        public static BHG.Vector CartesianAddition(BHG.Vector A, BHG.Vector B)
+        {
+            return Create.Vector(A.X + B.X, A.Y + B.Y, A.Z + B.Z);
+        }
+
+        public static BHG.Vector CartesianAddition(BHG.Vector A, double B)
+        {
+            return Create.Vector(A.X + B, A.Y + B, A.Z + B);
+        }
+
+        public static BHG.Point CartesianAddition(BHG.Point A, BHG.Point B)
+        {
+            return Create.Point(A.X + B.X, A.Y + B.Y, A.Z + B.Z);
+        }
+
+        public static BHG.Point CartesianAddition(BHG.Point A, double B)
+        {
+            return Create.Point(A.X + B, A.Y + B, A.Z + B);
+        }
+
+
+        public static BHG.Vector CartesianSubtraction(BHG.Vector A, BHG.Vector B)
+        {
+            return Create.Vector(A.X - B.X, A.Y - B.Y, A.Z - B.Z);
+        }
+
+        public static BHG.Vector CartesianSubtraction(BHG.Vector A, double B)
+        {
+            return Create.Vector(A.X - B, A.Y - B, A.Z - B);
+        }
+
+        public static BHG.Point CartesianSubtraction(BHG.Point A, BHG.Point B)
+        {
+            return Create.Point(A.X - B.X, A.Y - B.Y, A.Z - B.Z);
+        }
+
+        public static BHG.Point CartesianSubtraction(BHG.Point A, double B)
+        {
+            return Create.Point(A.X - B, A.Y - B, A.Z - B);
+        }
+
+
+        public static BHG.Vector CartesianMultiplication(BHG.Vector A, BHG.Vector B)
+        {
+            return Create.Vector(A.X * B.X, A.Y * B.Y, A.Z * B.Z);
+        }
+
+        public static BHG.Vector CartesianMultiplication(BHG.Vector A, double B)
+        {
+            return Create.Vector(A.X * B, A.Y * B, A.Z * B);
+        }
+
+        public static BHG.Point CartesianMultiplication(BHG.Point A, BHG.Point B)
+        {
+            return Create.Point(A.X * B.X, A.Y * B.Y, A.Z * B.Z);
+        }
+
+        public static BHG.Point CartesianMultiplication(BHG.Point A, double B)
+        {
+            return Create.Point(A.X * B, A.Y * B, A.Z * B);
+        }
+
+
+        public static BHG.Vector CartesianDivision(BHG.Vector A, BHG.Vector B)
+        {
+            return Create.Vector(A.X / B.X, A.Y / B.Y, A.Z / B.Z);
+        }
+
+        public static BHG.Vector CartesianDivision(BHG.Vector A, double B)
+        {
+            return Create.Vector(A.X / B, A.Y / B, A.Z / B);
+        }
+
+        public static BHG.Point CartesianDivision(BHG.Point A, BHG.Point B)
+        {
+            return Create.Point(A.X / B.X, A.Y / B.Y, A.Z / B.Z);
+        }
+
+        public static BHG.Point CartesianDivision(BHG.Point A, double B)
+        {
+            return Create.Point(A.X / B, A.Y / B, A.Z / B);
+        }
+    }
+}
