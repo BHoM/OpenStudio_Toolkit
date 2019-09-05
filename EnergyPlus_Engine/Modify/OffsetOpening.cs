8dca3b955fcab81d0d25b96e5693ec998db6aaac
Rasmus Canfjorden <rasmus.canfjorden@burohappold.com>
2019-09-05 16:56:16 +0100

    something

---
+++ EnergyPlus_Engine/Modify/OffsetOpening.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BHE = BH.oM.Environment.Elements;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.EnergyPlus
+{
+    public static partial class Modify
+    {
+        public static BHE.Panel OffsetOpening(BHE.Panel panel)
+        {
+            return null;
+        }
+    }
+}
