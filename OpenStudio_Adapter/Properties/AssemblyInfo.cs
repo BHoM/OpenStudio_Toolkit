0f0d8a4d3652dd7af17e425765d5aeaec13d641f
Rasmus Canfjorden <rasmus.canfjorden@burohappold.com>
2020-01-08 15:34:17 +0000

    Changes made according to comments

--- OpenStudio_Adapter/Properties/AssemblyInfo.cs
+++ OpenStudio_Adapter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EnergyPlus_Adapter")]
+[assembly: AssemblyTitle("OpenStudio_Adapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EnergyPlus_Adapter")]
+[assembly: AssemblyProduct("OpenStudio_Adapter")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- OpenStudio_oM/Properties/AssemblyInfo.cs
+++ OpenStudio_oM/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EnergyPlus_oM")]
+[assembly: AssemblyTitle("OpenStudio_oM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EnergyPlus_oM")]
+[assembly: AssemblyProduct("OpenStudio_oM")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
